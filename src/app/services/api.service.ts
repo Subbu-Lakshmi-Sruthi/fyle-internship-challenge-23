eaa9e2b1624e05e95c9bab157a47cdd1020da653
Subbu Lakshmi Sruthi MSR <subbulakshmi.m@hyperverge.co>
2024-04-14 20:00:58 +0530

    Added cacheing of username-page-perpagecount combination

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
   private apiUrl: string = 'https://api.github.com';
+  private cache = new Map<string, Observable<any[]>>();
 
   constructor(private http: HttpClient) { }
 
@@ -16,14 +17,25 @@ export class ApiService {
   }
 
   getUserRepos(username: string, page: number, perPage: number): Observable<any[]> {
-    return this.http.get<any[]>(`${this.apiUrl}/users/${username}/repos?page=${page}&per_page=${perPage}`).pipe(
+    const cacheKey = `${username}_${page}_${perPage}`;
+    
+    const cachedData = this.cache.get(cacheKey);
+    if (cachedData) {
+      return cachedData;
+    }
+
+    const repos$ = this.http.get<any[]>(`${this.apiUrl}/users/${username}/repos?page=${page}&per_page=${perPage}`).pipe(
       map(repos => repos.map(repo => ({
         name: repo.name,
         html_url: repo.html_url,
         description: repo.description,
         topics: repo.topics,
         public_repos: repo.public_repos
-      })))
+      }))),
+      shareReplay(1)
     );
+
+    this.cache.set(cacheKey, repos$);
+    return repos$;
   }
 }
